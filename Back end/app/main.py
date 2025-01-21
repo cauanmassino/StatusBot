03112fcaca693649366192c8796efddb880baaf3
Cauan Massino <cauanmassino55@gmail.com>
2025-01-21 14:15:42 -0300

    Update main.py

--- Back end/app/main.py
+++ Back end/app/main.py
@@ -10,7 +10,6 @@ CORS(app)
 # Rota para fornecer os dados fictícios das barras
 @app.route("/api/barras")
 def get_barras():
-    # Dados fictícios representando os dias e seus status
     barras = [
         {"dia": "Dia 1", "status": "ok"},
         {"dia": "Dia 2", "status": "outage"},
@@ -20,11 +19,6 @@ def get_barras():
     ]
     return jsonify(barras)
 
-# Rota inicial para verificar se a API está funcionando
-@app.route("/")
-def index():
-    return "API está funcionando!"
 
-# Ponto de entrada da aplicação
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
